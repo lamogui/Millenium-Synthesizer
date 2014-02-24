92c8dea3c766b4c1f57c5d2c7c69dcfd3e3b4941
Lamogui <julien.deloor@gmail.com>
2014-02-24 23:05:54 +0100

    Bon sa met du sinus 440Hz dans les hp
    Attention consomme l'intégralité du proco !
    La version avec callback n'est pas encore codé !

--- main.cpp
+++ main.cpp
@@ -15,9 +15,18 @@ int main()
   //sf::View cam(sf::Vector2f(0,0),sf::Vector2f(terrain.width + 1,6.0*terrain.width/8.0));
   sf::RenderWindow window(sf::VideoMode(800, 600), "Millenium Synth");
   
-  window.setFramerateLimit(50);
+  BASS_Init(-1, Signal::frequency, 0,0,0);
+  
+  //window.setFramerateLimit(50); //Detruit tout ne pas activer
   float dt=0.02;
-
+  
+  Signal s;
+  HSTREAM stream = s.createCompatibleBassStream(false);
+  std::cout << "bass start " << BASS_Start() << std::endl;
+  std::cout << "bass channel start " << BASS_ChannelPlay(stream,TRUE) << std::endl;
+  
+  int delta=0;
+  
   while (window.isOpen())
   {
   // Process events
@@ -45,6 +54,25 @@ int main()
           break;
       }
     }
+    
+    
+    if (s.prepareNextBuffer())
+    {
+      //std::cout << "prepare\n"; 
+      sample* samples = s.getPreparedBuffer();
+      for (int i=0;i < Signal::size-1;i+=2)
+      {
+        samples[i] = 0.3*sin(2.0*3.1415*441.0*(delta + i/2)/(float)Signal::frequency);
+        samples[i+1] = samples[i];
+      }
+      delta+=Signal::size/2;
+    }
+    
+    if (s.pushInBass())
+    {
+      //std::cout << "pushed in bass\n"; 
+    }
+    
     // Update the window
     window.display();
   }
--- signal.hpp
+++ signal.hpp
@@ -4,7 +4,8 @@
 #include <vector>
 #include <cstdlib> //old malloc
 #include <cstring> //memset
-#include <bass.h>
+#include "bass.h"
+#include <SFML/System.hpp>
 
 typedef float sample; //type pour un echantillon
 
@@ -12,34 +13,59 @@ class Signal
 {
   public:
     Signal();
-    Signal(const Signal& s);
+    //COPY CONSTRUCTOR IS NON-THREAD SAFE
+    Signal(const Signal& s); //don't copy the bass stream associated ! need to regenerate it !
     virtual ~Signal();  
     
     //Constants for all signals
     static const unsigned int frequency = 44100;
     static const unsigned int channels = 2; //stereo only supported yet
-    static const unsigned int refreshRate = 50; 
+    static const unsigned int refreshRate = 50; //Le but c'est d'avoir 50 ici ! le plus possible sera le mieux
     static const unsigned int size = channels*frequency/refreshRate;
     static const unsigned int byteSize = sizeof(sample)*size;
-    static const unsigned int nBuffers = 4; //min 2
+    static const unsigned int nBuffers = 4; //minimum 3 le but c'est d'en avoir le moins possible (ça augmente la latence)
     
     
     //Get an access to the buffers
+    //NON-THREAD SAFE BUT YOU SHOULD NOT WRITE WITH MULTIPLES THREADS
+    //(SHOULD BE) SAFE WITH READING (WILL NOT ERASE THE CURRENT PLAYED BUFFER !)
     sample* getPreparedBuffer(); //get current prepared buffer
     bool prepareNextBuffer(); //finnish prepare the current buffer
     
-    //Play the buffer
-    bool pop(); //Remove the first in buffer from the queue
-    bool playInBass(HSTREAM stream); //push the current buffer in bass stream
+    //Play the playable buffer
+    //DON'T USE 3 FOLLOWING WITH BASS CALLBACK STREAM !
+    bool pushInBass(); //push the current buffer in bass stream (push method)
+    sample* usePlayableBuffer(); //use the current played buffer DON'T USE WITH BASS CALLBACK STREAM !
+    void releasePlayableBuffer(); //release the current playable buffer and go to the next
     
-    //Create the bass stream
-    static HSTREAM CreateCompatibleBassStream();
+    //DON'T forget to RELEASE the PlayableBuffer ! AND DON'T BE TIME EXCESSIVE BETWEEN USE AND RELEASE
+    
+    
+    //Create the bass stream erase previous created
+    HSTREAM createCompatibleBassStream(bool callback=true); //use callback method -> direct played into speakers and no need 
+    HSTREAM getBassStream(); //getter
       
   protected:
+    void _pop(); //Remove the first in buffer from the queue (because it has been played)
+  
     std::vector<sample*> _buffers; //fifo/circular signal buffers
     unsigned int _prepared;  //Current prepared buffer
     unsigned int _played;   //Current played buffer
     unsigned int _filled;   //Number of filled buffer
+    HSTREAM _stream;        //Bass Stream
+    
+  private:
+    //Callback mutex
+    sf::Mutex _callbackMutex;
+    
+    //Delta into a buffer for _BassStreamProc
+    unsigned int _relativeDelta;
+  
+    //The Bass Stream proc available 
+    static DWORD CALLBACK _BassStreamProc(HSTREAM handle,
+                                          void *buffer,
+                                          DWORD length,
+                                          void *user);
 };
 
 
