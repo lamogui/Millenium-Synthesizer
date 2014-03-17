b76889e125a4c96ff8e0bd549fca8ed71c936562
Lamogui <julien.deloor@gmail.com>
2014-03-17 23:23:31 +0100

    construction de step enveloppe

--- enveloppe.cpp
+++ enveloppe.cpp
@@ -2,3 +2,49 @@
 #include "enveloppe.hpp"
 
 
+ void Enveloppe::step(Signal* output)
+ {
+    if (_releaseTime)
+    {
+      //release
+      for (int i=0;i < Signal::size-1;i+=2)
+      {
+
+        _time++;
+      }
+    }
+    else
+    {
+      if (_time < attack)
+      {
+        //attack
+        for (int i=0;i < Signal::size-1;i+=2)
+        {
+
+          _time++;
+        }
+      }
+      else if (_time < attack + decay)
+      {
+        //decay
+        for (int i=0;i < Signal::size-1;i+=2)
+        {
+
+          _time++;
+        }
+      }
+      else
+      {
+        for (int i=0;i < Signal::size-1;i+=2)
+        {
+
+          _time++;
+        }
+        //sustain
+      }
+      
+      //toujours pas de release time ! on met à jour la valeur à laquel il va commencer
+      _releaseLevel = output[Signal::size-1]; 
+    }
+    
+ }
