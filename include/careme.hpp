f2f90c2eccb1ee72697716aa5f1edc1c6e713f0c
kwon-young <kwon-young.choi@hotmail.fr>
2014-03-24 21:05:02 +0100

    ajout de l'interface graphique de careme
    Former-commit-id: 52f89df47706064970d4367b552d44c93f4990e5

--- include/careme.hpp
+++ include/careme.hpp
@@ -4,6 +4,12 @@
 #include "instrument.hpp"
 #include "oscillator.hpp"
 #include "interface.hpp"
+#include "enveloppe.hpp"
+
+#define PARAM_CAREME_ENVATTACK 1
+#define PARAM_CAREME_ENVDECAY 2
+#define PARAM_CAREME_ENVSUSTAIN 3
+#define PARAM_CAREME_ENVRELEASE 4
 /*
 #define PARAM_CAREME_LFOAMP_ID 1
 #define PARAM_CAREME_LFOFREQUENCY_ID 2
@@ -22,23 +28,44 @@ class CaremeVoice : public InstrumentVoice
     virtual void step(Signal* output);
     
   protected:
-    SinusoidalOscillator _osc1;
-    SinusoidalOscillator _osc2;
-    SinusoidalOscillator _osc3;
+    SinusoidalOscillator* _osc1;
+    SinusoidalOscillator* _osc2;
+    SinusoidalOscillator* _osc3;
+    Enveloppe _env;
 };
-/*
+
 class Careme : public Instrument<CaremeVoice>
 {
   public:
     Careme();
     virtual ~Careme();
   
-  virtual InstrumentParameter* getParameter(unsigned char id);
+    virtual InstrumentParameter* getParameter(unsigned char id);
+    virtual void step(Signal* output);
   
   protected:
+    InstrumentParameter _env_attack;
+    InstrumentParameter _env_decay;
+    InstrumentParameter _env_sustain;
+    InstrumentParameter _env_release;
+    /*
     InstrumentParameter _lfo_amp;
     InstrumentParameter _lfo_frequency;
     InstrumentParameter _duty;
+    */
+};
+class CaremeKnob : public Knob
+{
+  public:
+    CaremeKnob(InstrumentParameter* p, const sf::Texture &texture, const sf::IntRect &backRect, const sf::IntRect &knobRect);
+    virtual ~CaremeKnob();
+    
+    void update();
+    void draw (sf::RenderTarget &target, sf::RenderStates states) const;
+  
+  private:
+    sf::RectangleShape _selector;
+    sf::RectangleShape _movingSelector;
 };
 
 class CaremeInterface : public Interface
@@ -50,11 +77,17 @@ class CaremeInterface : public Interface
   protected:
     sf::Texture _texture;
     sf::Sprite _back;
-    Knob* _lfoAmpKnob;
-    Knob* _lfoFrequencyKnob;
-    Knob* _dutyKnob; 
     Careme* _instrument;
+    /*
+    CaremeKnob* _lfoAmpKnob;
+    CaremeKnob* _lfoFrequencyKnob;
+    CaremeKnob* _dutyKnob; 
+    */
+    CaremeKnob* _envAttackKnob;
+    CaremeKnob* _envDecayKnob;
+    CaremeKnob* _envSustainKnob;
+    CaremeKnob* _envReleaseKnob;
 };
-*/
+
 #endif
 
