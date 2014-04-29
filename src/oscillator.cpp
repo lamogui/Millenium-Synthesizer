04d81076b6a7e72f9c5404ecb57a87f9bb4f58f0
kwon-young <kwon-young.choi@hotmail.fr>
2014-04-29 13:09:15 +0200

    correction du blanc
    Former-commit-id: eca363e8db80546a9870e14e7c38a81989447552

--- src/oscillator.cpp
+++ src/oscillator.cpp
@@ -173,7 +173,7 @@ void WhiteNoiseOscillator::step(Signal* output)
 {
   sample* samples = output->samples;
   std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(-1,1); 
+  std::uniform_real_distribution<float> distribution(-1.0,1.0); 
 
   for (unsigned int i=0;i < Signal::size;i++)
   {
