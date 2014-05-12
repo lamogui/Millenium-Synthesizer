1f64c954e9ec876aca4add8c7c31fd0250f4518e
m1cadore <maximecadoret56@gmail.com>
2014-05-12 08:47:31 +0200

    Ajout cado
    Former-commit-id: 9720467d49cd70ec3e68278e5be1596857fa2c88

---
+++ include/cado.hpp
@@ -0,0 +1,54 @@
+#ifndef __CADO
+#define __CADO
+
+#include "instrument.hpp"
+#include "oscillator.hpp"
+#include "interface.hpp"
+#include "enveloppe.hpp"
+#include "filter.hpp"
+
+
+
+class CadoVoice : public InstrumentVoice
+{
+  public:
+    CadoVoice(AbstractInstrument* creator);
+    virtual ~CadoVoice();
+
+    void beginNote(Note& n);
+    void endNote();
+    void step(Signal* leftout, Signal* rightout=0);
+    
+  private:
+    Oscillator* _osc1;
+    Enveloppe _env;
+    Note _currentNote;
+};
+
+
+class Cado : public Instrument<CadoVoice>
+{
+  public: 
+    Cado();
+    virtual ~Cado();
+    
+    void step(Signal* leftout, Signal* rightout=0);
+    
+}; 
+
+
+class CadoInterface : public Interface
+{
+  public:
+    CadoInterface(Cado* instrument, const sf::Vector2f& size);
+    virtual ~CadoInterface();
+    
+  protected:
+    sf::Texture _texture;
+    sf::Sprite _back;
+    Cado* _instrument;
+};
+
+
+
+#endif
