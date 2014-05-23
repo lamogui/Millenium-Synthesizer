dd49e709b64923ab3e1b471c32c4f63233294a0b
Lamogui <julien.deloor@gmail.com>
2014-05-23 22:07:07 +0200

    Ajout du code pour les preset
    Former-commit-id: c4af70afb1653cacc200a2ece0c07dab1d6f5f4e

---
+++ src/preset.cpp
@@ -0,0 +1,80 @@
+
+#include "preset.hpp"
+
+Preset::Preset() : 
+_buffer(0),
+_size(0)
+{
+
+}
+
+Preset::~Preset() 
+{
+  if (_buffer) 
+    free(_buffer);
+}
+
+
+unsigned int Preset::read_from_buffer(const unsigned char* buffer, 
+                                      unsigned int size,
+                                      unsigned int& offset)
+{
+  const unsigned int previous_offset=offset;
+  if (size >= offset + 4) return 0;
+  if (   buffer[offset] == 'P' && buffer[offset] == 'R'
+      && buffer[offset] == 'S' && buffer[offset] == 'T')
+  {
+    offset+=4;
+    if (size - offset)
+    {
+      _size = size - offset;
+      _buffer=(unsigned char*)realloc((void*)_buffer,_size);
+      memcpy((void*)_buffer,(void*)(buffer + offset),_size);
+    }
+  }
+  return offset - previous_offset;
+}
+
+unsigned int Preset::write_to_buffer( unsigned char* buffer, 
+                                      unsigned int size,
+                                      unsigned int& offset) const
+{
+  if (size < offset + byte_size()) return 0;
+  buffer[offset++]='P';
+  buffer[offset++]='R';
+  buffer[offset++]='S';
+  buffer[offset++]='T';
+  memcpy((void*)(buffer + offset),(void*)_buffer,_size);
+  offset+=_size;
+  return size+4;
+}
+
+bool Preset::pack(AbstractInstrument* instrument)
+{
+  if (!instrument) return false;
+  _buffer=(unsigned char*)realloc((void*)_buffer,3*255); //1 BYTE + 1 WORD
+  _size=0;
+  
+  for (unsigned int i=0; i < 255; i++)
+  {
+    InstrumentParameter* p;
+    if (p=instrument->getParameter(i)) {
+      _buffer[_size++]=i;
+      _buffer[_size++]=p->getValue() >> 8;
+      _buffer[_size++]=p->getValue() & 0xFF;
+    }
+  }
+  return true;
+}
+
+bool Preset::unpack(AbstractInstrument* instrument)
+{
+  if (!_buffer ||!_size || !instrument) return false;
+  unsigned offset=0;
+  for (unsigned int i=0; i < _size/3; i++)
+  {
+    instrument->setParameterValue( _buffer[offset++],
+                                  (_buffer[offset++]<<8) | 
+                                  (_buffer[offset++]));
+  }
+}
